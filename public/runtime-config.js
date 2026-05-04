b8374c6ab577a50d9347a80f7f15d50d2e4fda83
Thanh-Nguyen-Valerie <thanh.nguyen@skill-wanderer.com>
2026-05-04 15:48:46 +0700

    chore: remove runtime-config.js and obsolete test from git tracking

--- public/runtime-config.js
+++
@@ -1,7 +0,0 @@
-window.__env = Object.assign({
-  "KEYCLOAK_URL": "https://sso.skill-wanderer.com",
-  "KEYCLOAK_REALM": "skill-wanderer-admin",
-  "KEYCLOAK_ADMIN_CLIENT_ID": "skill-wanderer-admin",
-  "KEYCLOAK_GOOGLE_IDP_HINT": "google",
-  "APP_WEBSITE_URL": "https://skill-wanderer.com"
-}, window.__env ?? {});
